c28455edcf67d8c89c8e743b37058a0f78c7eda3
loldmenl <oldmen@test.com>
2022-03-05 14:29:24 +0000

    - card type regexs were simplified

--- domain/src/main/java/com/exercise/domain/use_case/GetCardTypeUseCase.kt
+++ domain/src/main/java/com/exercise/domain/use_case/GetCardTypeUseCase.kt
@@ -30,13 +30,13 @@ class GetCardTypeUseCase : UseCase<String, CardType> {
     }
 
     private companion object {
-        val jcbRegex = Regex("^(?:2131|1800|35)[0-9]{0,}$")
-        val ameRegex = Regex("^3[47][0-9]{0,}\$")
-        val dinersRegex = Regex("^3(?:0[0-59]{1}|[689])[0-9]{0,}\$")
-        val visaRegex = Regex("^4[0-9]{0,}\$")
-        val masterCardRegex = Regex("^(5[1-5]|222[1-9]|22[3-9]|2[3-6]|27[01]|2720)[0-9]{0,}\$")
-        val maestroRegex = Regex("^(5[06789]|6)[0-9]{0,}\$")
+        val jcbRegex = Regex("^(?:2131|1800|35)[0-9]*$")
+        val ameRegex = Regex("^3[47][0-9]*\$")
+        val dinersRegex = Regex("^3(?:0[0-59]|[689])[0-9]*\$")
+        val visaRegex = Regex("^4[0-9]*\$")
+        val masterCardRegex = Regex("^(5[1-5]|222[1-9]|22[3-9]|2[3-6]|27[01]|2720)[0-9]*\$")
+        val maestroRegex = Regex("^(5[06789]|6)[0-9]*\$")
         val discoverRegex =
-            Regex("^(6011|65|64[4-9]|62212[6-9]|6221[3-9]|622[2-8]|6229[01]|62292[0-5])[0-9]{0,}\$")
+            Regex("^(6011|65|64[4-9]|62212[6-9]|6221[3-9]|622[2-8]|6229[01]|62292[0-5])[0-9]*\$")
     }
 }
